fabe8b3e57fdeb4e3c2ffb93191c476e498dd514
narenkarn786@gmail.com <narenkarn786@gmail.com>
2023-08-09 13:57:37 +0545

    Added Swagger Documentation

--- ResourceInventory/Program.cs
+++ ResourceInventory/Program.cs
@@ -1,8 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using ResourceInventory.Model;
+using ResourceInventory.Model.CategoryModel;
 using ResourceInventory.Service.CategoryService;
 using ResourceInventory.Service.ProductService;
 using ResourceInventory.Service.SubProductService;
+using Swashbuckle.AspNetCore.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,10 +14,19 @@ builder.Services.AddDbContext<ApplicationDBContext>(option => option.UseSqlServe
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<ICategoryRepository,CategoryRepository>();
-builder.Services.AddScoped<IProductRepository,ProductRepository>();
-builder.Services.AddScoped<ISubProductRepository,SubProductRepository>();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "ResourceInventoryAPI",
+        Version = "v1",
+        Description = "ResourceInventoryAPI is a RESTful API that manages resources and inventory data. It provides endpoints to create, retrieve, update, and delete resources and inventory items."
+    });//API Documentation
+
+});
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ISubProductRepository, SubProductRepository>();
 
 var app = builder.Build();
 
@@ -21,7 +34,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "ResourceInventory V1");
+        
+    });
 }
 
 app.UseHttpsRedirection();
@@ -31,3 +48,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
