7e6de03586d41955fc9a67ac96b079746a2e8f19
Naren Karn <62215176+narenkarn786@users.noreply.github.com>
2023-08-13 09:20:13 +0545

    Productand sub product (#4)
    * update and delete of sub product
    
    * return ko chai mailey sir variable nai banayera garey sir
    
    ---------
    
    Co-authored-by: KushalTamang-Alive <kushal.tamang@iic.edu.np>

--- ResourceInventory/Controllers/SubProductController.cs
+++ ResourceInventory/Controllers/SubProductController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ResourceInventory.Model.ProductModel;
 using ResourceInventory.Model.SubProductModel;
+using ResourceInventory.Service.ProductService;
 using ResourceInventory.Service.SubProductService;
 
 namespace ResourceInventory.Controllers
@@ -33,6 +35,24 @@ namespace ResourceInventory.Controllers
            
         }
 
+        [HttpGet, Route("GetSubProductByID")]
+        public async Task<IActionResult> GetSubProductById(int id)
+        {
+            try
+            {
+                var result = await _subProductRepository.GetSubProductById(id);
+                if (result == null)
+                    return BadRequest("Data not found");
+                return Ok(result);
+
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error Retrieving Sub-Product");
+            }
+
+        }
+
 
         [HttpPost,Route("AddSubProduct")]
         public async Task<IActionResult> AddSubProduct(SubProductDto subProduct)
@@ -51,6 +71,51 @@ namespace ResourceInventory.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, "Error inserting new subproduct");
             }
 
-            }          
+        }
+
+        [HttpPut,Route("UpdateSubProduct")]
+        public async Task<IActionResult> UpdateSubProduct(SubProduct subProduct, int id)
+        {
+            try
+            {
+                if (id != subProduct.Id)
+                {
+                    return BadRequest("Sub-Product ID Mismatch");
+                }
+
+           
+                var subProductToBeUpdated = await _subProductRepository.GetSubProductById(id);
+                if (subProductToBeUpdated == null)
+                {
+                    return NotFound($"Sub-Product with Id={subProduct.Id} not found");
+                }
+
+                var updatedSubProduct = await _subProductRepository.UpdateSubProduct(subProduct);
+                return Ok(updatedSubProduct);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error updating Sub-Product");
+            }
+        }
+
+        [HttpDelete, Route("Delete-SubProduct")]
+        public async Task<IActionResult> DeleteSubProduct(int id)
+        {
+            try
+            {
+                var subProductToDelete = await _subProductRepository.GetSubProductById(id);
+                if (subProductToDelete == null)
+                {
+                    return NotFound($"Sub-Product with Id={id} not found");
+                }
+                await _subProductRepository.DeleteSubProduct(id);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error deleting Sub-Product");
+            }
+        }
     }
 }
